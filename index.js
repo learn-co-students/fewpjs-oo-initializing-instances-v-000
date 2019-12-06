f15f620450ac54525090df41a2403841317b70d6
Richard Cho <richardcho100@gmail.com>
2019-12-05 22:16:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink)  {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+/* 
+Dinner will have a constructor with salad, soup, entree, and dessert.
+Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
+*/
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
