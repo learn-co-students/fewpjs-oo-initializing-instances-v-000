89c5b603d09983b128f02a6780a349e6b5855b9e
John Blank <veloblank@gmail.com>
2020-01-06 23:58:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 class Breakfast {
-	constructor(food, drink) {
-		this.food = food;
-		this.drink = drink;
-	}
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
 }
 
 class Lunch {
-	constructor(salad, soup, drink) {
-		this.salad = salad;
-		this.soup = soup;
-		this.drink = drink;
-	}
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
 }
 
 class Dinner {
-	constructor(salad, soup, entree, dessert) {
-		this.salad = salad;
-		this.soup = soup;
-		this.entree = entree;
-		this._dessert = dessert;
-	}
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
 }
