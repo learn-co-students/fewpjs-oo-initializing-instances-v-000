e54e87ce3e8423ee2a885fb6c080d774b4a610ac
ElvinGarcia <dimix181@gmail.com>
2020-04-20 16:22:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+
+class Breakfast {
+    constructor(food, drink){
+        this.food = food ;
+        this.drink = drink ;
+    }
+}   
+
+class Lunch {
+    constructor (salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup,entree,_dessert){
+    this._dessert = _dessert ;
+    this.entree = entree;
+    this.soup = soup;
+    this.salad = salad;
+ }
+}
