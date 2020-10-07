35ebc1956666ce713a75aca2b358f34f33e1c55d
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-07 18:22:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+let bfast = new Breakfest("egg", "juice");
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+let newlunch = new Lunch("salad", "broccoli cheddar soup", "iced tea");
+
+// class Dinner{
+//     constructor(){
+
+//     }
+// }
